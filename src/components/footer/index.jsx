8448d46c2f90ee42657fa8d20149ed6e3cb4c516
thumbsu <me@thumbsu.dev>
2020-09-02 16:29:04 +0900

    FIX Footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-
 import './index.scss'
+
+import React from 'react'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
+    ©<a href="https://github.com/thumbsu">Thumbsu</a>, Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
