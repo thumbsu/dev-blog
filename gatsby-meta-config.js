7fbd78f482c5b2e286b4a42917867590214b9390
thumbsu <me@thumbsu.dev>
2020-09-02 14:35:15 +0900

    ADD resume

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `미래의 엄지수를 위한 개발 타임캡슐`,
   author: `thumbsu`,
   introduction: `React Native로 앱 개발 하고 있어요. 딱히 가리는 것 없습니다.`,
-  siteUrl: `https://thumbsu.netlify.com`, // Your blog site url
+  siteUrl: `https://thumbsu.dev`, // Your blog site url
   social: {
     twitter: `___IDUN`, // Your Twitter account
     github: `thumbsu`, // Your GitHub account
@@ -12,7 +12,7 @@ module.exports = {
   icon: `content/assets/favicon.jpg`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'thumbsu/dev-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
